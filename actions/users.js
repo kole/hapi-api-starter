d94bd48e57349bdd77e44c8b0c259ecda867ae3b
Nick Johnson <nickj224@gmail.com>
2016-12-06 18:09:40 -0500

    signup uses promises... much nicer

--- actions/users.js
+++ actions/users.js
@@ -7,54 +7,74 @@ class Users extends MongoModels {
     // as part of signup, create a new user in the database
     static create(user, cb) {
         const usr = user;
+        const signupFunctions = [
+            this.usernameCheck(usr.username),
+            this.emailCheck(usr.email),
+            this.passwordHash(usr.password)
+        ];
 
-        // enforce unique username
-        this.uniqueUsername(usr.username, (existingUser) => {
-            if (existingUser) {
-                return cb(Boom.conflict('Username is already in use'));
-            }
+        Promise.all(signupFunctions).then((done) => {
+            const password = done[2];
+            this.insertUser(usr, password).then((userObj) => {
+                return cb(userObj);
+            });
+        }).catch((err) => {
+            return cb(err);
+        });
+    }
 
-            // enforce unique email address
-            return this.uniqueEmail(usr.email, (existingEmail) => {
-                if (existingEmail) {
-                    return cb(Boom.conflict('Email is already registered'));
+    static emailCheck(email) {
+        return new Promise((resolve, reject) => {
+            this.findOne({ email }, (err, result) => {
+                if (result) {
+                    return reject(Boom.conflict('Email is already in use'));
                 }
-
-                return bcrypt.hash(usr.password, 10).then((hash) => {
-                    // prep doc for insertion
-                    const doc = Object.assign(usr, {
-                        _id: user.userId,
-                        password: hash,
-                        createdAt: moment(moment()).unix()
-                    });
-
-                    // userId was moved to '_id', so we don't need it here anymore
-                    delete doc.userId;
-
-                    return this.insertOne(doc, (err, result) => {
-                        if (err) { throw new Error(err); }
-
-                        const userFromDB = result[0];
-
-                        // don't return the password to the requesting function
-                        delete userFromDB.password;
-
-                        return cb(userFromDB);
-                    });
-                });
+                return resolve(result);
             });
         });
     }
 
-    static uniqueEmail(email, cb) {
-        this.findOne({ email }, (err, result) => {
-            return cb(result);
+    static insertUser(user, password) {
+        return new Promise((resolve, reject) => {
+            // prep doc for insertion
+            const doc = Object.assign(user, {
+                _id: user.userId,
+                password,
+                createdAt: moment(moment()).unix()
+            });
+
+            // userId was moved to '_id', so we don't need it here anymore
+            delete doc.userId;
+
+            return this.insertOne(doc, (err, result) => {
+                if (err) { return reject(Boom.badRequest('Database error')); }
+
+                const userFromDB = result[0];
+
+                // don't return the password to the requesting function
+                delete userFromDB.password;
+
+                return resolve(userFromDB);
+            });
         });
     }
 
-    static uniqueUsername(username, cb) {
-        this.findOne({ username }, (err, result) => {
-            return cb(result);
+    static passwordHash(password) {
+        return new Promise((resolve) => {
+            return bcrypt.hash(password, 10).then((hash) => {
+                resolve(hash);
+            });
+        });
+    }
+
+    static usernameCheck(username) {
+        return new Promise((resolve, reject) => {
+            this.findOne({ username }, (err, result) => {
+                if (result) {
+                    return reject(Boom.conflict('Username is already in use'));
+                }
+                return resolve(result);
+            });
         });
     }
 }
